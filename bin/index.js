9e5bd234842923461db006ad276c9619aa50d1a6
Safia <safiaali@hotmail.com>
2021-06-17 00:24:30 +0100

    Add functions to create menu system. Add book to reading list, display reading list. WIP

--- bin/index.js
+++ bin/index.js
@@ -2,23 +2,9 @@
 
 const chalk = require("chalk");
 const boxen = require("boxen");
-const yargs = require("yargs");
 const axios = require("axios");
 const prompt = require("prompt-sync")();
-
-// const options = yargs
-//   .usage('Usage: -n <name>')
-//   .option('n', {
-//     alias: 'name',
-//     describe: 'Your name',
-//     type: 'string',
-//     demandOption: false,
-//   })
-//   .option('s', {
-//     alias: 'search',
-//     describe: 'Search term',
-//     type: 'string',
-//   }).argv;
+const fs = require('fs');
 
 const boxenOptions = {
   padding: 1,
@@ -30,84 +16,178 @@ const boxenOptions = {
 
 const greeting = chalk.white.bold("Welcome to 8th Light's Google Books CLI API");
 
-// const userName = `Hello, ${options.name}!`
-
 const msgBox = boxen(greeting, boxenOptions);
 console.log(msgBox);
+mainMenu();
 
-const name = prompt(chalk.white.bold('What is your name? '));
-console.log(chalk.red.bold(`Hey there ${name} `));
+function mainMenu() {
 
-const search = prompt(chalk.white.bold('What book would you like to search for? '));
-console.log(chalk.red.bold(`Searching for books about: ${search} `));
-// console.log(userName);
+    const menu = prompt(
+      chalk.white(
+        'Select Option & Press ENTER \n 1: Search for books \n 2: View Reading List \n 3: Exit \n')
+    );
+    console.log(
+      chalk.white(`You have selected option  `) + chalk.red(`${menu} \n`)
+    );
 
-// if (options.search) {
-//   console.log(`Searching for books about ${options.search}...`);
-// } else {
-//   console.log("Here's a random book for you:");
-// }
+    if(`${menu}`== 1 ){
+        const search = prompt(
+          chalk.white('What book would you like to search for? ')
+        );
+        console.log(
+          chalk.white(`Searching for books about: `) + chalk.red(`${search} \n`)
+        );
+        getBookDetails(search);
 
-// let keyword = 'sun';
+    } else if (`${menu}` == 2) {        // view readingList.JSON
+        console.log('call getReadingList()');
 
-// const url = options.search   ? `https://www.googleapis.com/books/v1/volumes?q=${options.search}&printType=books&startIndex=0&maxResults=4&projection=lite`
-//   : `https://www.googleapis.com/books/v1/volumes?q=${keyword}&printType=books&startIndex=0&maxResults=4&projection=lite`;
+    } else if (`${menu}` == 3) {        // view readingList.JSON
+        process.exit(1);
+    } else {
+        console.log(
+          chalk.red.bold(`Option not available. Please try again! \n
+          `)
+        );
+        mainMenu();
+    }
+}
 
-const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&startIndex=0&maxResults=4&projection=lite`
+function searchMenu(bookArray) {
 
-axios.get(url)
-    .then(response => {
-        console.log(chalk.blue(`Book Title:`) + ` ${response.data.items[0].volumeInfo.title}`);
-        console.log(chalk.blue(`Author(s):`) + ` ${response.data.items[0].volumeInfo.authors}`);
-        console.log(chalk.blue(`Publisher:`) + ` ${response.data.items[0].volumeInfo.publisher}`);
+    const searchMenu = prompt(
+        chalk.white(
+            'Select Option \n 1: Save book to reading list \n 2: Exit to main menu'
+        )
+    );
+    console.log(
+        chalk.white(`You have selected option  `) + chalk.red(`${searchMenu} \n`)
+    );
 
-        // console.log({
-        //     books: result.data.items});
-    })
-    .catch(error => { console.error(`error: ${error}`) });
+    if (`${searchMenu}` == 1) {
+        const chosenBookId = prompt(
+            chalk.white('Insert the book number you would like to save to your reading list?')
+        );
+        console.log(
+            chalk.white(`Saving book number `) + chalk.red(`${chosenBookId} \n`)
+        );
 
-//      .then((response) => {
-//             if (!response.ok) throw new Error(response.status);
-//             return response.json();
-//           })
-//  .catch(error => { console.log(`error: ${error}`) });
+        if (`${chosenBookId}` > 0 && `${chosenBookId}` < 6) {
+          let chosenBook = bookArray.find((x) => x.menuID == `${chosenBookId}`);
+          // console.log(JSON.stringify(chosenBook));
+          saveToReadingList(chosenBook);
+        } else {
+          console.log(
+            chalk.red
+              .bold(`Book number not available. Please pick a number between 1-5! \n
+          `)
+          );
+          searchMenu();
+        }
 
-
-
-
-
-// Second approach
-// axios
-//   .get(url)
-//     .then((response) => {
-//          if (options.search) {
-//            // if searching for jokes, loop over the results
-//              response.data.results.forEach((id) => {
-//                 console.log(
-//                   chalk.blue(`Book Title:`) +
-//                     ` ${response.data.items[0].volumeInfo.title}`
-//                 );
-//                 console.log(
-//                   chalk.blue(`Author(s):`) +
-//                     ` ${response.data.items[0].volumeInfo.authors}`
-//                 );
-//                 console.log(
-//                   chalk.blue(`Publisher:`) +
-//                     ` ${response.data.items[0].volumeInfo.publisher}`
-//                 );
+    } else if (`${searchMenu}` == 2) {
+        mainMenu();
+    } else {
+        console.log(
+            chalk.red.bold(`Option not available. Please try again! \n
+          `)
+        );
+        searchMenu();
+    }
+}
 
 
-//             //  console.log('\n' + j.joke);
-//            });
-//            if (response.data.results.length === 0) {
-//              console.log("no books found :'(");
-//            }
-//          } else {
-//            console.log(response.data);
-//          }
-//     // console.log({
-//     //     books: result.data.items});
-//   })
-//   .catch((error) => {
-//     console.error(`error: ${error}`);
-//   });
+function saveToReadingList(chosenBook) {
+let readingListJSON = readFromReadingListJSONFile();
+
+readingListJSON.readingList.push(chosenBook);
+let data = JSON.stringify(readingListJSON, null, 2);
+fs.writeFile('readingList.json', data, finished);
+
+  function finished(err) {
+    console.log('Saved to reading list!');
+  }
+    
+  getreadinglist();
+
+}
+
+function readFromReadingListJSONFile() {
+    const file = fs.readFileSync('readingList.json');
+    return JSON.parse(file);
+}
+
+
+function getreadinglist() {
+
+    let display = readFromReadingListJSONFile();
+    console.log(`Your reading list: `);
+    display.readingList.forEach(item => {
+
+        console.log(` Title: ${item.title} + Author(s): ${item.author} + Publisher: ${item.publisher} `)
+    });
+
+    readingListMenu();
+
+}
+
+
+function readingListMenu(){
+
+    const readingListMenu = prompt(
+      chalk.white(
+        'Select Option & Press ENTER \n 1: Back to main menu \n 2: Exit \n')
+    );
+    console.log(
+      chalk.white(`You have selected option  `) + chalk.red(`${readingListMenu} \n`)
+    );
+
+    if(`${readingListMenu}`== 1 ){
+        mainMenu();
+
+    } else if (`${readingListMenu}` == 2) {        // view readingList.JSON
+        process.exit(1);
+    } else {
+        console.log(
+          chalk.red.bold(`Option not available. Please try again! \n
+          `)
+        );
+        readingListMenu();
+}
+
+function getBookDetails(search) {
+
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&startIndex=0&maxResults=5&projection=lite`
+
+    const searchResult = axios.get(url)
+        .then(response => {
+            let searchArray = response.data.items
+
+                var bookArray= [];
+
+            for (let i = 0; i < searchArray.length; i++) {
+
+            var bookObj = searchArray[i].volumeInfo;
+
+                var book ={
+                    menuID: i+1,
+                    title: bookObj.title,
+                    author: bookObj.authors,
+                    publisher: bookObj.publisher
+                };
+            bookArray.push(book);
+
+
+                let id = searchArray[i].id;
+                let title = bookObj.title;
+                let author = bookObj.authors;
+                let publisher = bookObj.publisher;
+
+                console.log(
+                    chalk.blue.bold(`Book Number:`) + ` ${[i + 1,]} Book id: ${id} Title: ${title} Author(s): ${author} Publisher: ${publisher} `
+                );
+            }
+            searchMenu(bookArray);
+        })
+        .catch(error => { console.error(`error: ${error}`) });
+}
