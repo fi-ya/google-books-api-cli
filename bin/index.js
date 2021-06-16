4d081468a4b8dbd4e9cf93344514e20b29629442
Safia <safiaali@hotmail.com>
2021-06-16 11:07:17 +0100

    Install chalk and boxen to make change text colour and create borders

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('Hello!');
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const greeting = chalk.white.bold("Welcome to 8th Light's Google Books CLI API")
+
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "#555555"
+};
+
+const msgBox = boxen(greeting, boxenOptions);
+console.log(msgBox);
+
+
