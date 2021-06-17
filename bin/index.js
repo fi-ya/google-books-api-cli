ab51465efdbb81842cae08a28b85d01cd42d66bb
Safia <safiaali@hotmail.com>
2021-06-17 12:15:43 +0100

    fix chalk styling. fix edge cases, if incorrect option chosen WIP

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
-const boxen = require("boxen");
-const axios = require("axios");
-const prompt = require("prompt-sync")();
+const chalk = require('chalk');
+const boxen = require('boxen');
+const axios = require('axios');
+const prompt = require('prompt-sync')();
 const fs = require('fs');
 
 const boxenOptions = {
@@ -14,201 +14,221 @@ const boxenOptions = {
   backgroundColor: '#555555',
 };
 
-const greeting = chalk.white.bold("Welcome to 8th Light's Google Books CLI API");
+const greeting = chalk.white.bold(
+  "Welcome to 8th Light's Google Books CLI API"
+);
 
 const msgBox = boxen(greeting, boxenOptions);
 console.log(msgBox);
+
+console.log(
+  chalk.bold.inverse.cyanBright(`Hello!`) +
+    chalk.cyan(
+      `\nWelcome to my command line application that allows you to use the Google Books API to search for books and construct a reading list.`
+    ) +
+    chalk.cyan(
+      `\nUse your keyboard to enter the number of the option you would like from the menu shown below.`
+    ) +
+    chalk.cyan.bold(`\nHave fun finding interesting new things to explore! `)
+);
+
 mainMenu();
 
 function mainMenu() {
+  console.log(chalk.greenBright.inverse.bold('\nMain Menu\n'));
+  console.log(
+    chalk.white(
+      'Select an option: \n 1: Search for Books \n 2: View Reading List \n 3: Exit\n'
+    )
+  );
+  const menu = prompt();
+  // console.log(
+  //   chalk.white(`\nYou have selected option:  `) + chalk.red(`${menu} `)
+  // );
 
-    console.log(chalk.greenBright.inverse.bold('\nMain Menu\n'));
+  if (`${menu}` == 1) {
+     console.log(chalk.white('\nWhat book would you like to search for? '));
+     let search = prompt();
+
+      if (`${search}` === "") {
+          console.log(
+            chalk.red('\nNothing selected. Please enter the book or phrase you would like to search.  ')
+          );
+        console.log(chalk.white('\nWhat book would you like to search for? '));
+         search = prompt();
+      }
+
+    console.log(chalk.cyan.inverse(`\nBooks about '${search}': `));
+    getBookDetails(search);
+  } else if (`${menu}` == 2) {
+    getReadingList();
+  } else if (`${menu}` == 3) {
+    const exitGreeting = chalk.white.bold(`Have a great day. Goodbye! 😊 `);
+    const msgBox2 = boxen(exitGreeting, boxenOptions);
+    console.log(msgBox2);
+    process.exit(1);
+  } else {
     console.log(
-      chalk.white(
-        'Select an option: \n 1: Search for Books \n 2: View Reading List \n 3: Exit\n')
-    );
-    const menu = prompt();
-    // console.log(
-    //   chalk.white(`\nYou have selected option:  `) + chalk.red(`${menu} `)
-    // );
-
-    if(`${menu}`== 1 ){
-        console.log(
-          chalk.white('\nWhat book would you like to search for? ')
-        );
-        const search = prompt();
-        console.log(
-          chalk.cyan.inverse(`\nBooks about '${search}': `)
-        );
-        getBookDetails(search);
-
-    } else if (`${menu}` == 2) {
-        getReadingList();
-
-    } else if (`${menu}` == 3) {
-        const exitGreeting =
-          chalk.white.bold(`Have a great day. Goodbye! 😊 `)
-        ;
-        const msgBox2 = boxen(exitGreeting, boxenOptions);
-        console.log(msgBox2);
-        process.exit(1);
-    } else {
-        console.log(
-          chalk.red.bold(`\nOption not available. Please try again!
+      chalk.red.bold(`\nOption not available. Please try again!
           `)
-        );
-        mainMenu();
-    }
+    );
+    mainMenu();
+  }
 }
 
 function searchMenu(bookArray) {
-    console.log(chalk.greenBright.inverse.bold('Search Menu\n'));
+  console.log(chalk.greenBright.inverse.bold('Search Menu\n'));
+  console.log(
+    chalk.white(
+      'Select an option: \n 1: Save book to reading list \n 2: Back to main menu\n '
+    )
+  );
+  const searchMenu = prompt();
+  // console.log(
+  //   chalk.white(`\nYou have selected option  `) + chalk.red(`${searchMenu}`)
+  // );
+
+  if (`${searchMenu}` == 1) {
     console.log(
       chalk.white(
-        'Select an option: \n 1: Save book to reading list \n 2: Back to main menu\n '
+        '\nInsert the book number you would like to save to your reading list? '
       )
     );
-    const searchMenu = prompt();
-    // console.log(
-    //   chalk.white(`\nYou have selected option  `) + chalk.red(`${searchMenu}`)
-    // );
+      let chosenBookId = prompt();
 
-    if (`${searchMenu}` == 1) {
-        console.log(
-          chalk.white(
-            '\nInsert the book number you would like to save to your reading list? '
-          )
-        );
-        const chosenBookId = prompt();
-        console.log(
-          chalk.white(`\nSaving book number `) + chalk.red(`${chosenBookId}`)
-        );
+    console.log(
+      chalk.white(`\nSaving book number `) + chalk.red(`${chosenBookId}`)
+    );
 
-        if (`${chosenBookId}` > 0 && `${chosenBookId}` < 6) {
-          let chosenBook = bookArray.find((x) => x.menuID == `${chosenBookId}`);
-            if(chosenBook) saveToReadingList(chosenBook);
-
-        } else {
-          console.log(
-            chalk.red
-              .bold(`\nBook number not available. Please pick a number between 1-5!
-          `)
-          );
-          searchMenu(bookArray);
-        }
-
-    } else if (`${searchMenu}` == 2) {
-        mainMenu();
+    if (`${chosenBookId}` > 0 && `${chosenBookId}` < 6) {
+      let chosenBook = bookArray.find((x) => x.menuID == `${chosenBookId}`);
+      if (chosenBook) saveToReadingList(chosenBook);
     } else {
-        console.log(
-          chalk.red.bold(`Option not available. Please try again!\n
+      console.log(
+        chalk.red
+          .bold(`\nBook number not available. Please pick a number between 1-5!
           `)
-        );
-        searchMenu(bookArray);
+      );
+          //console.log(
+            //chalk.white(
+             //'\nInsert the book number you would like to save to your reading list? '
+            //)
+          //);
+          //chosenBookId = prompt();
+
+      // not working error: TypeError: searchMenu is not a function
+      //searchMenu(bookArray);
     }
+  } else if (`${searchMenu}` == 2) {
+    mainMenu();
+  } else {
+    console.log(
+      chalk.red.bold(`Option not available. Please try again!\n
+          `)
+    );
+    //mainMenu();
+    searchMenu(bookArray);
+  }
 }
 
 function saveToReadingList(chosenBook) {
-let readingListJSON = readFromReadingListJSONFile();
+  let readingListJSON = readFromReadingListJSONFile();
 
-readingListJSON.readingList.push(chosenBook);
-let data = JSON.stringify(readingListJSON, null, 2);
-fs.writeFile('readingList.json', data, finished);
+  readingListJSON.readingList.push(chosenBook);
+  let data = JSON.stringify(readingListJSON, null, 2);
+  fs.writeFile('readingList.json', data, finished);
 
   function finished(err) {
     console.log('\nSaved to reading list!');
     getReadingList();
   }
-
 }
 
 function readFromReadingListJSONFile() {
-    const file = fs.readFileSync('readingList.json');
-    return JSON.parse(file);
+  const file = fs.readFileSync('readingList.json');
+  return JSON.parse(file);
 }
 
 function getReadingList() {
+  let display = readFromReadingListJSONFile();
+  console.log(chalk.red.bold.inverse(`\nYour reading list: `));
+  display.readingList.forEach((item) => {
+    console.log(
+      chalk.red.bold(` \n Title:`) +
+        chalk.white(` ${item.title} `) +
+        chalk.red.bold(` Author(s):`) +
+        chalk.white(` ${item.author} `) +
+        chalk.red.bold(` Publisher: `) +
+        chalk.white(` ${item.publisher}`)
+    );
+  });
 
-    let display = readFromReadingListJSONFile();
-    console.log(chalk.red.bold.inverse(`\nYour reading list: `));
-    display.readingList.forEach(item => {
-
-        console.log(
-                    chalk.red.bold(` \n Title:`) +
-                    chalk.white(` ${item.title} `) +
-                    chalk.red.bold(` Author(s):`) +
-                    chalk.white(` ${item.author} `) +
-                    chalk.red.bold(` Publisher: `) +
-                    chalk.white(` ${item.publisher}`)
-        );
-    });
-    
-    readingListMenu();
+  readingListMenu();
 }
 
-function readingListMenu(){
-    console.log(chalk.greenBright.inverse.bold('\nReading List Menu\n'));
+function readingListMenu() {
+  console.log(chalk.greenBright.inverse.bold('\nReading List Menu\n'));
+  console.log(
+    chalk.white('Select an option: \n 1: Back to main menu \n 2: Exit \n')
+  );
+  const readingListMenu = prompt();
+  // console.log(
+  //   chalk.white(`\nYou have selected option  `) +
+  //     chalk.red(`${readingListMenu} `)
+  // );
+
+  if (`${readingListMenu}` == 1) {
+    mainMenu();
+  } else if (`${readingListMenu}` == 2) {
+    const exitGreeting = chalk.white.bold(`Have a great day. Goodbye! 😊 `);
+    const msgBox2 = boxen(exitGreeting, boxenOptions);
+    console.log(msgBox2);
+    process.exit(1);
+  } else {
     console.log(
-      chalk.white('Select an option: \n 1: Back to main menu \n 2: Exit \n')
-    );
-    const readingListMenu = prompt();
-    // console.log(
-    //   chalk.white(`\nYou have selected option  `) +
-    //     chalk.red(`${readingListMenu} `)
-    // );
-
-    if(`${readingListMenu}`== 1 ){
-        mainMenu();
-
-    } else if (`${readingListMenu}` == 2) {
-        const exitGreeting = chalk.white.bold(`Have a great day. Goodbye! 😊 `);
-        const msgBox2 = boxen(exitGreeting, boxenOptions);
-        console.log(msgBox2);
-        process.exit(1);
-    } else {
-        console.log(
-          chalk.red.bold(`\nOption not available. Please try again!
+      chalk.red.bold(`\nOption not available. Please try again!
           `)
-        );
-        readingListMenu();
-    }
+    );
+    mainMenu();
+  }
 }
 
 function getBookDetails(search) {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&startIndex=0&maxResults=5&projection=lite`;
 
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&printType=books&startIndex=0&maxResults=5&projection=lite`
+  const searchResult = axios
+    .get(url)
+    .then((response) => {
+      let searchArray = response.data.items;
 
-    const searchResult = axios.get(url)
-        .then(response => {
-            let searchArray = response.data.items
+      var bookArray = [];
 
-                var bookArray= [];
+      for (let i = 0; i < searchArray.length; i++) {
+        var bookObj = searchArray[i].volumeInfo;
 
-            for (let i = 0; i < searchArray.length; i++) {
+        var book = {
+          menuID: i + 1,
+          title: bookObj.title,
+          author: bookObj.authors,
+          publisher: bookObj.publisher,
+        };
 
-            var bookObj = searchArray[i].volumeInfo;
+        bookArray.push(book);
 
-                var book ={
-                    menuID: i+1,
-                    title: bookObj.title,
-                    author: bookObj.authors,
-                    publisher: bookObj.publisher
-                };
-
-                bookArray.push(book);
-
-                console.log(
-                  chalk.white.bold(`\n Book Number:`) +
-                    chalk.blue.bold(` ${[book.menuID]}`) +
-                    chalk.red.bold(` Title:`) +
-                    chalk.white(` ${book.title} `) +
-                    chalk.red.bold(` Author(s):`) +
-                    chalk.white(` ${book.author} `) +
-                    chalk.red.bold(` Publisher: `) +
-                    chalk.white(` ${book.publisher} \n`)
-                );
-            }
-            searchMenu(bookArray);
-        })
-        .catch(error => { console.error(`error: ${error}`) });
+        console.log(
+          chalk.white.bold(`\n Book Number:`) +
+            chalk.cyan.bold(` ${[book.menuID]}`) +
+            chalk.red.bold(` Title:`) +
+            chalk.white(` ${book.title} `) +
+            chalk.red.bold(` Author(s):`) +
+            chalk.white(` ${book.author} `) +
+            chalk.red.bold(` Publisher: `) +
+            chalk.white(` ${book.publisher} \n`)
+        );
+      }
+      searchMenu(bookArray);
+    })
+    .catch((error) => {
+      console.error(`error: ${error}`);
+    });
 }
